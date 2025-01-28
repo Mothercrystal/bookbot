5de11dbc1da57bda904e983904ba855de39fad0b
Mothercrystal <thomas.dieden@gmx.de>
2025-01-28 21:45:16 +0100

    removed double frankenstein

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+def main ():
+
+    with open("./books/frankenstein.txt") as f:
+        file_contents = f.read()
+    
+
+
+    words = file_contents.split()
+    print(len(words))
+
+if __name__ == "__main__":
+    main()
+
