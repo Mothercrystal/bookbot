b6f93e7628e8d9070156c795f46daefe92624fe7
Mothercrystal <thomas.dieden@gmx.de>
2025-01-29 15:33:57 +0100

    added lower case count all

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 def main ():
 
-    with open("./books/frankenstein.txt") as f:
+    with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-    
 
 
     words = file_contents.split()
     print(len(words))
+
+
+    letters = file_contents.lower()
+    #print(letters)
+
+    lower_case_dict = {}
+    for i in letters:
+        if not i.isalpha():
+            continue
+
+        if i not in lower_case_dict:
+            lower_case_dict[i] = 1
+
+        else:
+            lower_case_dict[i] += 1   
+
+    print(lower_case_dict)
+            
 
 if __name__ == "__main__":
     main()
