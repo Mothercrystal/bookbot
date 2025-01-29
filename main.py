197257fcb8382a23e70a33f86e54d6f10a099197
Mothercrystal <thomas.dieden@gmx.de>
2025-01-29 17:32:44 +0100

    lesson 11 complete, added count and sort

--- main.py
+++ main.py
@@ -9,21 +9,36 @@ def main ():
 
 
     letters = file_contents.lower()
-    #print(letters)
+
 
     lower_case_dict = {}
     for i in letters:
         if not i.isalpha():
             continue
-
         if i not in lower_case_dict:
             lower_case_dict[i] = 1
-
         else:
             lower_case_dict[i] += 1   
 
-    print(lower_case_dict)
-            
+    
+
+    def sort_on(lower_case_dict):
+        return lower_case_dict["num"]
+
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{len(words)} words found in the document\n")
+
+    char_list = []
+    for char, count in lower_case_dict.items():
+        char_dict = {"letter" : char, "num" : count}
+        char_list.append(char_dict)
+
+    char_list.sort(key=sort_on, reverse=True)
+
+    for char_info in char_list:
+        print(f"The '{char_info["letter"]}' was found {char_info["num"]} times")
+
+    print("--- End report ---")
 
 if __name__ == "__main__":
     main()
